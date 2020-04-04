59180508524ed472f9eb72ce95db15109f59ad1c
Jason Miller <developit@google.com>
2020-04-03 21:35:19 -0400

    speed up builds by avoiding running react-icons through babel

--- preact.config.js
+++ preact.config.js
@@ -6,14 +6,22 @@ export default {
   webpack(config, env, helpers) {
     const DefinePlugin = helpers.getPluginsByName(config, "DefinePlugin")[0];
     DefinePlugin.plugin.definitions["process.env.SSR"] = String(env.ssr);
-    const { plugins } = config.module.rules.filter(({ loader }) => loader === "babel-loader")[0].options;
-    plugins.push([
+
+    const babelLoader = config.module.rules.filter(({ loader }) => loader === "babel-loader")[0];
+
+    // inject styled-components ssr plugin:
+    babelLoader.options.plugins.push([
       require.resolve("babel-plugin-styled-components"),
       {
         displayName: !env.isProd,
         ssr: env.ssr
       }
     ]);
+
+    // expensive/pointless modules to run through Babel:
+    const skipParsing = /(node_modules|~)\/(react-icons)\//gi;
+    babelLoader.exclude = [skipParsing].concat(babelLoader.exclude.exclude || []);
+
     config.plugins.push(
       new CopyWebpackPlugin([
         {
